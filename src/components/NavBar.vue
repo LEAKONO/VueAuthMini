c9432b5da3ca10e69e0fbf86b1538a6b1a1f8eec
LEAKONO <leakonoemmanuel3@gmail.com>
2025-05-30 21:32:01 +0300

    🐛 Fix route and component resolution issues

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav>
-        <router-link to="/signup">signup</router-link>
-        <router-link to="/login">Login</router-link>
-    </nav>
+  <nav>
+    <router-link to="/signup">Signup</router-link> |
+    <router-link to="/login">Login</router-link>
+  </nav>
 </template>
+
 <style scoped>
-nav{
 nav {
   padding: 1rem;
   background: #42b983;
@@ -17,7 +17,5 @@ a {
   color: white;
   margin: 0 1rem;
   text-decoration: none;
-}
-</style>
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,14 @@ import SignupPage from '../views/SignupPage.vue';
 import LoginPage from '../views/LoginPage.vue';
 
 const routes = [
+  { path: '/', redirect: '/signup' }, 
   { path: '/signup', component: SignupPage },
   { path: '/login', component: LoginPage },
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+export default router;
