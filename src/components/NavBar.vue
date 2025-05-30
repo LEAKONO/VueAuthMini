cedefd9b2577e9f522eb77fc5cc38a4579150a67
LEAKONO <leakonoemmanuel3@gmail.com>
2025-05-30 20:48:06 +0300

    🧩 Add NavBar component and mount router-view in App.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav>
+        <router-link to="/signup">signup</router-link>
+        <router-link to="/login">Login</router-link>
+    </nav>
+</template>
+<style scoped>
+nav{
+nav {
+  padding: 1rem;
+  background: #42b983;
+  color: white;
+  text-align: center;
+}
+
+a {
+  color: white;
+  margin: 0 1rem;
+  text-decoration: none;
+}
+</style>
+}
+</style>
